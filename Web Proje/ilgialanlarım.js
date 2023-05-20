0d77fdbb4279393fae6bbaf0eb84504d5abe605d
Sümeyye Karataş <karatasumeyye35@gmail.com>
2023-05-20 23:35:36 +0300

    Son1

--- Web Proje/ilgialanlarım.js
+++ Web Proje/ilgialanlarım.js
@@ -1,58 +1,48 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7708e720e619b90938d24795ca3fea33&page=1';
+const API_KEY = '7708e720e619b90938d24795ca3fea33';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=7708e720e619b90938d24795ca3fea33&query=';
+const main = document.getElementById('movieList');
 
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const main = document.getElementById('main');
+// Film listesi
+const movieList = ['Yüzüklerin Efendisi: Yüzük Kardeşliği (2001)', 'Yüzüklerin Efendisi: İki Kule (2002)','Harry Potter ve Ateş Kadehi (2005)','Yeşil Yol','Forrest Gump','3 Aptal','Rüzgarı Dizginleyen Çocuk','Can Dostum','Akıl Oyunları','Dangal',
+'Wednesday','Stranger Things','Annenizle Nasıl Tanıştım(2005)','Brooklyn Nine-Nine','Sherlock','Annenizle Nasıl Tanıştım (2005)'];       
 
-getMovies(API_URL);
+getMovies(movieList);
 
-async function getMovies(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-
-  console.log(data.results);
-  showMovies(data.results);
+async function getMovies(movieList) {
+  try {
+    for (const movieTitle of movieList) {
+      const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieTitle}`;
+      const response = await fetch(searchURL);
+      const data = await response.json();
+      if (data.results.length > 0) {
+        const movie = data.results[0];
+        showMovie(movie);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+function showMovie(movie) {
+  const { title, poster_path, vote_average, overview } = movie;
 
-  const searchTerm = search.value;
+  const movieEl = document.createElement('div');
+  movieEl.classList.add('movie');
 
-  if (searchTerm && searchTerm !== '') {
-    getMovies(SEARCH_API + searchTerm);
+  movieEl.innerHTML = `
+    <img src="${IMG_PATH + poster_path}" alt="${title}" />
+    <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>${title} <small>Overview</small></h3>
+      <p>${overview}</p>
+    </div>
+  `;
 
-    search.value = '';
-  } else {
-    window.location.reload();
-  }
-});
-
-function showMovies(movies) {
-  main.innerHTML = '';
-
-  movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
-
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
-
-    movieEl.innerHTML = `
-      <img src="${IMG_PATH + poster_path}" alt="${title}" />
-      <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-      </div>
-      <div class="overview">
-        <h3>${title} <small>Overview</small></h3>
-        <p>${overview}</p>
-      </div>
-    `;
-
-    main.appendChild(movieEl);
-  });
+  main.appendChild(movieEl);
 }
 
 function getClassByRate(vote) {
